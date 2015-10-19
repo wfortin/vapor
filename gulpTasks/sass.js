a86e064bb4a166ce6bf3b5eb2c63db2d4b0456f7
William Fortin <wfortin@coveo.com>
2015-10-19 15:53:43 -0400

    Switch all LESS files to SCSS

---
+++ gulpTasks/sass.js
@@ -0,0 +1,48 @@
+var config = require('./configuration');
+
+var gulp = require('gulp-help')(require('gulp'));
+var gulpif = require('gulp-if');
+var gutil = require('gulp-util');
+var gzip = require('gulp-gzip');
+var rename = require('gulp-rename');
+
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+
+var csscomb = require('gulp-csscomb');
+
+var autoprefixerOptions = config.autoprefixerOptions;
+var gzipOptions = config.gzipOptions;
+
+var useMinifiedSources = gutil.env.min;
+var useGzippedSources = gutil.env.gzip;
+
+gulp.task('sass', 'Compile sass files to dist folder', ['sprites'], function () {
+    return gulp.src('./less/guide.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(rename('CoveoStyleGuide.css'))
+        .pipe(sourcemaps.write('../css'))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(gulpif(useMinifiedSources, minifyCSS({
+            keepSpecialComments: 0,
+            processImport: false
+        })))
+        .pipe(gulpif(useGzippedSources, gzip(gzipOptions)))
+        .pipe(gulpif(useMinifiedSources, rename('CoveoStyleGuide.min.css')))
+        .pipe(gulpif(useMinifiedSources, gulp.dest('./dist/css')));
+});
+
+gulp.task('less:docs', 'Build less and copy it to /docs/dist', ['less'], function () {
+    gulp.src('./dist/css/*').pipe(gulp.dest('./dist/css')).pipe(gulp.dest('./_gh_pages/dist/css'));
+});
+
+
+gulp.task('less:format', function() {
+    return gulp.src('./less/**/*.less')
+        .pipe(csscomb())
+        .pipe(gulp.dest('./less'));
+});
